6a623f05a8fc25524350f943449a6df6c27b3f37
seijdeleon <seijdeleon@lbl.gov>
2025-09-12 15:04:47 -0700

    make selected data field optional

--- src/components/Tiled/OpenTiledRow.tsx
+++ src/components/Tiled/OpenTiledRow.tsx
@@ -11,8 +11,9 @@ export interface OpenTiledRowProps {
     handleReverseSortChange: (reverse: boolean) => void;
     showApiKeyInput?: boolean;
     showReverseSortInput?: boolean;
+    showSelectedData?: boolean;
 }
-export default function OpenTiledRow({userInputApiKey, setIsViewerOpen, buttonModeText, selectedData, handleInputChange, userInputReverseSort, handleReverseSortChange, showApiKeyInput, showReverseSortInput}: OpenTiledRowProps) {
+export default function OpenTiledRow({userInputApiKey, setIsViewerOpen, buttonModeText, selectedData, handleInputChange, userInputReverseSort, handleReverseSortChange, showApiKeyInput, showReverseSortInput, showSelectedData}: OpenTiledRowProps) {
 
     return (
         <span className="flex gap-2 items-center">
@@ -41,9 +42,14 @@ export default function OpenTiledRow({userInputApiKey, setIsViewerOpen, buttonMo
                 />
                 </>
              )}
+
             <button className="bg-blue-600 hover:bg-blue-500 text-white rounded-md px-2 py-1 h-fit ml-4" onClick={()=>setIsViewerOpen(true)}>{buttonModeText}</button>
-            <p className=" text-black text-nowrap overflow-x-auto max-w-72 min-w-36 w-fit bg-white hover:cursor-text px-1 py-1">{selectedData && selectedData.self.split("/").pop()}</p>
-            <ButtonCopyToClipboard size="small" showText={true} copyText={selectedData ? selectedData.self : ''} />
+            {showSelectedData && (
+                <>
+                <p className=" text-black text-nowrap overflow-x-auto max-w-72 min-w-36 w-fit bg-white hover:cursor-text px-1 py-1">{selectedData && selectedData.self.split("/").pop()}</p>
+                <ButtonCopyToClipboard size="small" showText={true} copyText={selectedData ? selectedData.self : ''} />
+                </>
+            )}
         </span>   
     )
 }
