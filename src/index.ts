37cd23662a435eaf0abd632700bfff0928155cd6
seijdeleon <seijdeleon@lbl.gov>
2025-11-10 12:50:15 -0800

    export all api functions for npm

--- src/index.ts
+++ src/index.ts
@@ -9,5 +9,31 @@ export type { TiledProps } from './components/Tiled/Tiled';
 //TYPES
 export type { TiledItemLinks, TiledSearchItem, TiledStructures, TiledTableRow, TableStructure, ArrayStructure, ContainerStructure, AwkwardStructure, AwkwardForm, SparseStructure } from './components/Tiled/types';
 //UTILS
-export {getSearchResults, getDefaultTiledUrl, getTableData, getFirstSearchWithApiKey, setBearerToken, generateFullImagePngPath, setReverseSort} from './components/Tiled/apiClient';
+export {
+  // Path management
+  setInitialPath,
+  getInitialPath,
+  
+  // Authentication and server configuration
+  setAuthErrorCallback,
+  getDefaultTiledUrl,
+  setBearerToken,
+  getServerInfo,
+  loginUserWithNamePassword,
+  
+  // Search and data retrieval
+  getSearchResults,
+  getFirstSearchWithApiKey,
+  getTableData,
+  getStructuredArrayData,
+  getXArrayData,
+  
+  // Image handling
+  generateFullImagePngPath,
+  getAuthenticatedImage,
+  
+  // Configuration and state management
+  setReverseSort,
+  resetGlobalState
+} from './components/Tiled/apiClient';
 export { isArrayStructure, isTableStructure, isContainerStructure} from './components/Tiled/types';
