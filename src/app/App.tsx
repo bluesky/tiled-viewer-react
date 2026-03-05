b1d188c09285cdbd6a0c38ca6aad55e881d6d5b7
seijdeleon <seijdeleon@lbl.gov>
2026-03-05 09:11:26 -0800

    add more test views

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -9,6 +9,7 @@ import { TestItemCollection } from '@/components/ManualTest/types';
 
 function App() {
 
+  console.log('App component rendered');
   const clearSavedTokensAndRefresh = () => {
     localStorage.removeItem('tiledAccessToken');
     localStorage.removeItem('tiledRefreshToken');
@@ -16,14 +17,36 @@ function App() {
   }
 
   const testItems: TestItemCollection = {
-    test1: {
-      name: 'Test that the Tiled viewer loads successfully with valid API key.',
-      element: <Tiled />
+    Demo1: {
+      name: 'Default View',
+      element: <Tiled />,
+      info: 'This is the default view which should render the viewer properly.',
+      command: 'tiled serve config tiled_demo.yml'
     },
-    test2: {
-      name: 'Test that the Tiled viewer shows an error message when an invalid API key is used.',
-      element: <Tiled isButtonMode={true} />
-    }
+    Demo2: {
+      name: 'Button Mode',
+      element: <Tiled  isButtonMode={true}/>,
+      info: 'This renders a button that when clicked should open up the viewer. Clicking outside the viewer should close it.',
+      command: 'tiled serve config tiled_demo.yml'
+    },
+    Api1: {
+      name: 'Button Mode with Api Key',
+      element: <Tiled isButtonMode={true} inButtonModeShowApiKeyInput={true} />,
+      info: 'This accepts an API key input in button mode. Note that once the API key has been set in the UI, the single Axios instance will hold onto it for any future calls. (Once you do this test, refresh the page otherwise the API key will be used for all other Tiled requests..)',
+      command: 'TILED_SINGLE_USER_API_KEY=test tiled serve config tiled_api_key.yml'
+    },
+    Auth1: {
+      name: 'Username Password Auth',
+      element: <Tiled />,
+      info: 'username: alice, password: test. This should display a login screen that allows a user to enter a username password to authenticate future requests',
+      command: 'ALICE_PASSWORD=test tiled serve config tiled_auth.yml'
+    },
+    // Auth2: {
+    //   name: 'OIDC Authentication',
+    //   element: <Tiled oidcRedirectUrl='http://localhost:5173' />,
+    //   info: 'This requires manual setup from the tester to link their ORCID account with keys and provide them to Tiled during startup. When working, the UI allows a user to follow OIDC flow in a new popup, then should automatically close and reload the component when complete',
+    //   command: 'tiled serve config tiled_orcid.yml'
+    // }
   };
 
   return (
