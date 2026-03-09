3e92bf4d95381b2b08c73ee31a106023fc756ecb
seijdeleon <seijdeleon@lbl.gov>
2026-03-09 10:25:13 -0700

    add oidc test

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -21,30 +21,36 @@ function App() {
       name: 'Default View',
       element: <Tiled />,
       info: 'This is the default view which should render the viewer properly.',
-      command: 'tiled serve config tiled_demo.yml'
+      command: 'tiled serve config tiled/tiled_demo.yml'
     },
     Demo2: {
       name: 'Button Mode',
       element: <Tiled  isButtonMode={true}/>,
       info: 'This renders a button that when clicked should open up the viewer. Clicking outside the viewer should close it.',
-      command: 'tiled serve config tiled_demo.yml'
+      command: 'tiled serve config tiled/tiled_demo.yml'
     },
     Api1: {
       name: 'Button Mode with Api Key',
       element: <Tiled isButtonMode={true} inButtonModeShowApiKeyInput={true} />,
-      info: 'This accepts an API key input in button mode. Note that once the API key has been set in the UI, the single Axios instance will hold onto it for any future calls. (Once you do this test, refresh the page otherwise the API key will be used for all other Tiled requests..)',
-      command: 'TILED_SINGLE_USER_API_KEY=test tiled serve config tiled_api_key.yml'
+      info: 'Enter "test" into the form. This accepts an API key input in button mode. Note that once the API key has been set in the UI, the single Axios instance will hold onto it for any future calls. (Once you do this test, refresh the page otherwise the API key will be used for all other Tiled requests..)',
+      command: 'TILED_SINGLE_USER_API_KEY=test tiled serve config tiled/tiled_api_key.yml'
     },
     Auth1: {
       name: 'Username Password Auth',
       element: <Tiled />,
       info: 'username: alice, password: test. This should display a login screen that allows a user to enter a username password to authenticate future requests',
-      command: 'ALICE_PASSWORD=test tiled serve config tiled_auth.yml'
+      command: 'ALICE_PASSWORD=test tiled serve config tiled/tiled_username_password.yml'
     },
-    // Auth2: {
-    //   name: 'OIDC Authentication',
-    //   element: <Tiled oidcRedirectUrl='http://localhost:5173' />,
-    //   info: 'This requires manual setup from the tester to link their ORCID account with keys and provide them to Tiled during startup. When working, the UI allows a user to follow OIDC flow in a new popup, then should automatically close and reload the component when complete',
+    Auth2: {
+      name: 'OIDC Flow',
+      element: <Tiled tiledBaseUrl='http://tiled.localhost:8000/api/v1' oidcRedirectUrl='http://localhost:5173'/>,
+      info: 'Tests if Tiled can follow OIDC flow. This is not going to work unless you provide env variables for ORCID as specified in the config.',
+      command: 'tiled serve config tiled/tiled_oidc.yml'
+    },
+    // Auth3: {
+    //   name: 'OIDC Authentication Central Tiled',
+    //   element: <Tiled tiledBaseUrl='https://tiled-staging.computing.als.lbl.gov/api/v1' oidcRedirectUrl='http://tiled-test:5174' />,
+    //   info: 'Run the vite app under tiled-test:5174 to get around CORS. Configure /etc/hosts to point to localhost for tiled-test.',
     //   command: 'tiled serve config tiled_orcid.yml'
     // }
   };
@@ -52,11 +58,11 @@ function App() {
   return (
     <>
       <section className="flex flex-col items-center justify-start h-screen w-screen relative">
-        <div className="mb-8">
+        <div className="absolute top-0 left-0 w-fit">
           <img 
             src={bluesky_logo} 
             alt="Bluesky Tiled Logo" 
-            className="h-64 w-auto mx-auto opacity-80 mt-8 hover:animate-pulse"
+            className="h-16 w-auto mx-auto opacity-80 hover:animate-pulse ml-2 mt-2"
           />
         </div>
         {/* <Tiled isButtonMode={true} size='medium' tiledBaseUrl='http://tiled.localhost:8000/api/v1'/>
